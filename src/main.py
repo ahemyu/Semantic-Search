baa4597e514572758d8ccddee5a884d3bdeecc1a
ahemyu <hansovic7@gmail.com>
2024-06-16 10:06:24 +0200

    exported hardcoded values into config file

--- src/main.py
+++ src/main.py
@@ -1,19 +1,26 @@
 import gradio as gr
 import pandas as pd
+import json
 from data_preprocessing import preprocess_data
 from embedding import run_embedding
 from search import SemanticSearch
 
+def load_config():
+    with open('../config.json', 'r') as config_file:
+        return json.load(config_file)
+
+config = load_config()
+
 def setup_environment():
-    cleaned_data_path = preprocess_data('../data/Semantic_Search_Test_Set_Electronic_Devices.csv')
-    run_embedding(cleaned_data_path, 'sentence-transformers/all-MiniLM-L6-v2', 384, 'electronic_products', "http://localhost:6333")
-    searcher = SemanticSearch('sentence-transformers/all-MiniLM-L6-v2', 'localhost', 6333, 'electronic_products')
+    cleaned_data_path = preprocess_data(config["data_file_path"])
+    run_embedding(cleaned_data_path, config["model_name"], config["vector_dimension"], config["collection_name"], f"http://{config['qdrant_host']}:{config['qdrant_port']}")
+    searcher = SemanticSearch(config["model_name"], config["qdrant_host"], config["qdrant_port"], config["collection_name"])
     return searcher
 
 searcher = setup_environment()
 
 def search_products(query):
-    results = searcher.search(query, limit=5)
+    results = searcher.search(query, limit=config["search_limit"])
     data = [{
         'Name': res['productName'],
         'Category': res['productCategory'],
